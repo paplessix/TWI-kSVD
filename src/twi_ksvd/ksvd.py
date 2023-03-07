fde4b5ad901852eeaccc574f4ff104f20656b8da
paplessix <pierre-adrien.plessix@mines-paristech.fr>
2023-03-07 11:44:44 +0100

    before merge

--- src/twi_ksvd/ksvd.py
+++ src/twi_ksvd/ksvd.py
@@ -4,37 +4,41 @@ from twi_ksvd.omp import OMP
 
 
 class kSVD():
-    def __init__(self, K = 10, epsilon  = 1e-3) -> None:
+    def __init__(self, K = 10, epsilon  = 1e-3, max_iter = 1000) -> None:
         self.K = K  #number of atoms 
         self.epsilon = epsilon
-
+        self.max_iter = max_iter
 
     def fit (self, X, D, tau):
         """
         
         """
-        max_iter = 100
-        n=0
-        N  = len(X) # number of input samples
+        N  = len(X[0]) # number of input samples
         alphas = []
 
         # Initialize values 
 
         E_k_old = 0 
         E_k = np.inf 
-
-        while abs(np.linalg.norm(E_k) - np.linalg.norm(E_k_old)) < self.epsilon : # Stopping Criterion
+        n_iter = 0
+        while abs(np.linalg.norm(E_k) - np.linalg.norm(E_k_old)) > self.epsilon: # Stopping Criterion
             
             # Compute sparse codes 
 
             for i in range(N):
-                alphas.append(OMP(X[i],D,tau))
-            A = np.vstack(alphas)
+                # alphas.append(OMP(X[i],D,tau))
+                alphas.append( np.ones((self.K,1)))
+            A = np.hstack(alphas)
 
             # Update dictionnary 
 
             for k in range(self.K):
-                E_k = X - D.delete(k).T@A.delete(k) #TODO: Check if it is really doing what it needs to do 
+                mask = np.ones(len(D), dtype=bool)
+                mask[k] = False
+                print(X.shape)
+                print(D.shape)
+                print(A.shape)
+                E_k = X - D[mask].T@A[mask] #TODO: Check if it is really doing what it needs to do 
                 Omega_k = np.where(A[k] != 0)[0]
                 E_k_restricted = E_k[Omega_k]
                 u, s, vh = np.linalg.svd(E_k_restricted, full_matrices=True)
@@ -43,6 +47,18 @@ class kSVD():
 
                 D[k,:] = u[0] 
                 A[k: ] = s[0]*vh[0]
-            
-            n+=1 
+
+            if n_iter > self.max_iter :
+                print(f"Maximum number of iteration reached : {self.max_iter}")
+                break
+            else: 
+                n_iter +=1
         return A, D
+
+if __name__ == '__main__':
+    model = kSVD( 10)
+    X = np.zeros((3,10))
+    D = np.zeros((3,10))
+    tau = 3
+    model. fit(X,D,tau )
+    print("tip")
