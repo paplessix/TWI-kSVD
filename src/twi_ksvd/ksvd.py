2148c055e87fae2ee16d02d2e4e6e359a5490dc5
paplessix <pierre-adrien.plessix@mines-paristech.fr>
2023-03-07 11:27:04 +0100

    First try implementation of ksvd

--- src/twi_ksvd/ksvd.py
+++ src/twi_ksvd/ksvd.py
@@ -1,8 +1,48 @@
 
+import numpy as np 
+from twi_ksvd.omp import OMP
 
 
-def kSVD(X, D, tau):
+class kSVD():
+    def __init__(self, K = 10, epsilon  = 1e-3) -> None:
+        self.K = K  #number of atoms 
+        self.epsilon = epsilon
 
-    A = ...
-    D = ...
-    return A, D
+
+    def fit (self, X, D, tau):
+        """
+        
+        """
+        max_iter = 100
+        n=0
+        N  = len(X) # number of input samples
+        alphas = []
+
+        # Initialize values 
+
+        E_k_old = 0 
+        E_k = np.inf 
+
+        while abs(np.linalg.norm(E_k) - np.linalg.norm(E_k_old)) < self.epsilon : # Stopping Criterion
+            
+            # Compute sparse codes 
+
+            for i in range(N):
+                alphas.append(OMP(X[i],D,tau))
+            A = np.vstack(alphas)
+
+            # Update dictionnary 
+
+            for k in range(self.K):
+                E_k = X - D.delete(k).T@A.delete(k) #TODO: Check if it is really doing what it needs to do 
+                Omega_k = np.where(A[k] != 0)[0]
+                E_k_restricted = E_k[Omega_k]
+                u, s, vh = np.linalg.svd(E_k_restricted, full_matrices=True)
+                
+                #Update values 
+
+                D[k,:] = u[0] 
+                A[k: ] = s[0]*vh[0]
+            
+            n+=1 
+        return A, D
