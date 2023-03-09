c7e6babf016904cc7bebcbb5bf7fa1a6e36c6708
paplessix <pierre-adrien.plessix@mines-paristech.fr>
2023-03-09 11:45:47 +0100

    Implement TWI-kSVD

--- src/twi_ksvd/ksvd.py
+++ src/twi_ksvd/ksvd.py
@@ -1,5 +1,6 @@
 
 import numpy as np 
+from tqdm import tqdm 
 from twi_ksvd.omp import OMP, TWI_OMP
 
 
@@ -28,17 +29,18 @@ class kSVD():
         E_k = np.inf 
         n_iter = 0
         while abs(np.linalg.norm(E_k) - np.linalg.norm(E_k_old)) > self.epsilon: # Stopping Criterion
-            
+            print("=============================================")
             print(f"iteration number : {n_iter}, eps : {abs(np.linalg.norm(E_k))}, delta_eps : {abs(np.linalg.norm(E_k) - np.linalg.norm(E_k_old))}")
             E_k_old = E_k  
             alphas = []            
-            
+            print("Compute Sparse Codes")
             # Compute sparse codes 
 
             for i in range(self.N):
                 alphas.append(OMP(X[:,i],D,tau))
             self.A = np.vstack(alphas).T
 
+            print("Update Dictionnary")
             # Update dictionnary 
 
             for k in range(self.K):
@@ -76,14 +78,15 @@ class TWI_kSVD():
         self.epsilon = epsilon  # precision
         self.max_iter = max_iter # max number of iterations
 
-    def rotation(a,b,c):
+    def rotation(self,a,b,c):
         theta =  np.arccos(np.clip(np.dot(b/np.linalg.norm(b), c/np.linalg.norm(c)), -1.0, 1.0))
         u = b / np.linalg.norm(b)
         v = (c- np.dot(u,c)*u)/np.linalg.norm((c- np.dot(u,c)*u))
         c, s = np.cos(theta), np.sin(theta)
         R_theta = np.array(((c, -s), (s, c)))
-        R = np.eye(len(u)) - np.outer(u,u) - np.outer(v,v) + np.vstack((u,v))@R_theta@np.vstack((u,v)).T
-
+        R = np.eye(len(u)) - np.outer(u,u) - np.outer(v,v) + np.vstack((u,v)).T@R_theta@np.vstack((u,v))
+        return R@a
+    
     def fit (self, X, D, tau):
         """
         """
@@ -91,54 +94,74 @@ class TWI_kSVD():
         self.N  = len(X[0]) # number of input samples
         # Initialize values 
 
-        E_k_old = 0 
-        E_k = np.inf 
+        E_old = 0 
+        E = np.inf 
         n_iter = 0
-        while abs(np.linalg.norm(E_k) - np.linalg.norm(E_k_old)) > self.epsilon: # Stopping Criterion
-            print(f"iteration number : {n_iter}, eps : {abs(np.linalg.norm(E_k))}, delta_eps : {abs(np.linalg.norm(E_k) - np.linalg.norm(E_k_old))}")
-            E_k_old = E_k  
+        while abs(np.linalg.norm(E) - np.linalg.norm(E_old)) > self.epsilon: # Stopping Criterion
+            print("=============================================")
+            print(f"iteration number : {n_iter}, eps : {abs(np.linalg.norm(E))}, delta_eps : {abs(np.linalg.norm(E) - np.linalg.norm(E_old))}")
+            E_old = E  
             # init
             self.alphas = []
             self.alignements = []
-
+            print("     Step 1 | Compute Sparse Codes")
             # Compute sparse codes 
 
-            for i in range(self.N):
+            for i in tqdm(range(self.N)):
                 alpha, delta_ij= TWI_OMP(X[:,i],D,tau)
                 self.alphas.append(alpha)
                 self.alignements.append(delta_ij)
-                       
-            
+            self.siblings_atoms = [ [self.alignements[i][k]@self.D[k,:] for k in range(self.K)] for i in range(self.N)]
+            self.alphas = np.vstack(self.alphas).T
+            # print(self.alphas.shape)
+
+            # Compute error
+            E = np.array([ X[:,i] - np.sum([self.alignements[i][j]@self.D[j,:]*self.alphas[j,i] for j in np.arange(self.K)]) for i in range(self.N)])
+            print(E.shape)
+            print("     Step 2 | Update Dictionnary")
+            # Update dictionnary
             for k in range(self.K):
-                mask = np.ones(len(self.D[0]), dtype=bool)
+                mask = np.ones(self.K, dtype=bool)
                 mask[k] = False
+                Omega_k = self.alphas[k,:] != 0
 
-                Omega_k = self.alphas[k][:] != 0
-                residuals= []
-
-                for i in Omega_k.nonzero():
-                        e_i = X[:,i] - np.sum([self.alignements[i]@self.D[:,j]*self.alphas[i,j] for j in np.arange(self.K)[mask]])
-                        rotated_res = self.rotation(self.alignements[i].T@e_i; self.alignements[i].T@self.alignements[k]D[:,k], self.D[:,k])
-                        residuals.append(rotated_res)
-                u, s, vh = np.linalg.svd(np.concatenate(residuals), full_matrices=True)
+                rotated_residuals= []
+                residuals = []
+                # print(Omega_k.nonzero()[0])
+                for i in Omega_k.nonzero()[0]:
+                        e_i = X[:,i] - np.sum([self.alignements[i][j]@self.D[j,:]*self.alphas[j,i] for j in np.arange(self.K)[mask]])
+                        residuals.append(e_i)
+                        rotated_res = self.rotation(self.alignements[i][k].T@e_i, self.alignements[i][k].T@self.siblings_atoms[i][k], self.D[k,:])
+                        # print(rotated_res.shape)
+                        rotated_residuals.append(rotated_res)
+                if sum(Omega_k) >= 2 :
+                    u, s, vh = np.linalg.svd(np.vstack(rotated_residuals).T, full_matrices=True)
+                    
+                    #  Update values 
+                    self.D[k,:] = u[0] 
+                    
+                    for index, i in  enumerate(Omega_k.nonzero()[0]):
+                        inv_rot_u = self.alignements[i][k]@self.rotation(u[0], self.D[k,:],self.alignements[i][k].T@ self.siblings_atoms[i][k])
+                        self.siblings_atoms[i][k] = inv_rot_u
+                        self.alphas[k,i] = np.dot(residuals[index], inv_rot_u)/np.linalg.norm(inv_rot_u)
                 
 
             
             
-            
+
             
             if n_iter > self.max_iter :
                 print(f"Maximum number of iteration reached : {self.max_iter}")
                 break
             else: 
                 n_iter +=1
-        return self.A, self.D
+        return self.alphas, self.D
     
 
 if __name__ == '__main__':
     model = TWI_kSVD( 10)
-    X = np.random.random((100,4))
-    D = np.random.random((100,10))
+    X = np.random.random((2048,4))
+    D = np.random.random((2048,10)).T
     tau = 3
     model. fit(X,D,tau )
     print("tip")
